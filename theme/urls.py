62407cc4675f8e01642b22b6416bf729cd367bd8
ajagabos007 <ajagabos007@gmail.com>
2023-08-10 06:56:40 +0100

    resetting project

--- theme/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'theme'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('blog-details/', views.blog_details, name='blog-details'),
-    path('index-2/', views.index2, name='index-2'),
-]
