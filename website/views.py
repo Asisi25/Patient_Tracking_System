9b482bfc371c304a937edd1296e7344d41335e08
ajagabos007 <ajagabos007@gmail.com>
2023-08-10 16:53:51 +0100

    add website index page

--- website/views.py
+++ website/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 
 # Website home page
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse("Hello, world. You're at our website home page.")
+    return render(request, "website/index.html")
